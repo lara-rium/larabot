d369ad2924e952a2cd810227bf2ae4489bc157de
Lara Kayaalp <me@lara.lv>
2026-02-05 05:21:14 +0300

    simplify eligible cache logic

--- lib/webhook.ex
+++ lib/webhook.ex
@@ -2,23 +2,20 @@ defmodule Larabot.Webhook do
   alias Larabot.Error
   alias Nostrum.Api.Channel
   alias Nostrum.Api.Webhook
-  alias Nostrum.Cache.Me
 
   def get_or_create(channel_id) do
-    name = "Larabot"
-
     webhooks =
       channel_id
       |> Channel.webhooks()
       |> Error.handle()
 
-    webhook = Enum.find(webhooks, &(&1.name == name && &1.user.id == Me.get().id))
+    webhook = Enum.find(webhooks, & &1.token)
 
     if webhook do
       webhook
     else
       channel_id
-      |> Webhook.create(%{name: name, avatar: nil})
+      |> Webhook.create(%{name: "Larabot", avatar: nil})
       |> Error.handle()
     end
   end
